7008f195ec036c1be1d2f5ea026ef6e977f1afbf
jmorgadopp <jmorgadopp@alumnos.unex.es>
2025-11-09 13:06:22 +0100

    feat: Implementado GA02-157 (#done)

--- stats/admin.py
+++ stats/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Playback, AlbumSale
-admin.site.register(Playback)
-admin.site.register(AlbumSale)
+from .models import Rating
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("user", "song_id", "artist_id", "stars", "rated_at")
+    list_filter = ("stars",)
+    search_fields = ("song_id", "artist_id", "user__username", "user__email")
